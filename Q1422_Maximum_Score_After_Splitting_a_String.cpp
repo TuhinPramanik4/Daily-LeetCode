46bb954e745123c7e83e6c19cea7cf167c653244
Tuhin Pramanik <156042548+TuhinPramanik4@users.noreply.github.com>
2025-01-01 17:40:42 +0530

    Solution of 1st jan

---
+++ Q1422_Maximum_Score_After_Splitting_a_String.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxScore(string s) {
+        int one = count(s.begin(),s.end(),'1');
+        int ans=0;
+        int zero=0;
+        for(int i=0;i<s.size()-1;i++){
+            if(s[i]=='1'){
+                one--;
+            }
+            else{
+                zero++;
+            }
+            ans = max(ans,zero+one);
+        }
+        return ans;
+    }
+};
